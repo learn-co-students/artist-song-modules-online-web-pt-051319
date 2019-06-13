9a7680b5a3c34ffe1a1380cb0a1b84469c50e7ce
Annie Truex <artruex@gmail.com>
2019-06-13 23:15:18 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+
+module Memorable
+  module ClassMethods
+  def reset_all
+    all.clear
+  end
+
+  def count
+    all.count
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
