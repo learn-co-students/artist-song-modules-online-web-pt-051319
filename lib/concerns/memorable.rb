dee23a51dbf3fb4370e3fe505f78d376962d5d9d
Jasmine Chen <jasmine1226@gmail.com>
2019-06-13 01:30:37 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+  module ClassModules
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+
+  module InstanceModules
+    def initialize
+      self.class.all << self
+    end
+  end
+end
